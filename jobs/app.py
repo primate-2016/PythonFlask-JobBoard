acca403b08ae8dbde6db7ee846e022abd7adf1d1
primate-2016 <richardwhit@sky.com>
2019-02-23 11:13:27 +0000

    display individual job

--- jobs/app.py
+++ jobs/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, g # g is Flask's global helper to enable database connection 
 import sqlite3
 
-PATH = 'db/jobs.sqlite' # point to the sqlite database
+PATH = 'db/jobs.sqlite' # constant to point to the sqlite database at this path
 
 # you can preview your work by running 'flask run' in the root of your repo. Then visit http://localhost:5000 in your browser.
-app = Flask(__name__) # an instance of the Flask class, pass in the __name__ of this module (i.e. app)
+app = Flask(__name__) # instantiate an instance of the Flask class, pass in the __name__ of this module (i.e. app)
 
 def open_connection(): # create a function to connect to the database
     connection = getattr(g, '_connection', None) # use the built-in gettattr() to get the _connection attribute of the 'g' object
@@ -35,3 +35,8 @@ def close_connection(exception):
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs) # tell flask to render the index.html template when this function is called (by hitting the above routes)
+
+@app.route('/job/<job_id>')
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
+    return render_template('job.html', job=job)
